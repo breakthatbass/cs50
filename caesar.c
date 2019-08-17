dfaf82f98f169982469b257825b9f155cf2cd324
breakthatbass <gamache.taylor@gmail.com>
2019-08-17 10:47:35 -0400

    caesar file progress

--- caesar.c
+++ caesar.c
@@ -1,16 +1,33 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
+// main takes two argumaents: an int and a string
 int main(int argc, string argv[])
 {
-    if (argc == 2)
+    string s;
+    int k = atoi(argv[1]);
+    // check to make sure there are only two arguments
+    if (argc == 2 )
     {
-        printf("Success\n");
+        // prompt the user to input a string to be converted
+        s = get_string("plaintext:");
+        
+        //iterate through each char in string
+        for(int i = 0; i < strlen(s); i++)
+        {
+            // convert each char to an int
+           int c = (int) s[i];
+           // add k to each char and print to terminal
+           printf("ciphertext: %c\n", c + k); // got a bug to fix here!!
+        }
         return 0;
     } else
     {
+        // unsure why "Usage: " is necessary here but it is what the directions asked for
         printf("Usage: ./caesar key\n");
         return 1;
     }
     
 }
+

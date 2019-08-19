2480bba87a13fc8c7b4c25798f1a9a15a3164d41
breakthatbass <gamache.taylor@gmail.com>
2019-08-19 17:58:06 -0400

    add buggy vigenere file

---
+++ vigenere.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// not working yet :(
+
+int main(int argc, string argv[])
+{
+    char * plaintext;
+    char * keyword = argv[1];
+    int c;
+    
+    if (argc != 2){
+        printf("Usage: ./vigenere keyword\n");
+        return 1;
+    } else
+    {
+        
+     // prompt the user to input a string to be converted
+        plaintext = get_string("plaintext:");
+
+        printf("ciphertext: ");
+        
+        //iterate through each char in string
+        for(int i = 0; i < strlen(plaintext); i++)
+        {
+            for(int j = 0; j < strlen(keyword); j++){
+             c = (int) (plaintext[i] + keyword[i]) % 26; 
+            }
+            //loop through keyword
+            //make keyword an int
+           
+           
+           printf("ciphertext: %c", c);
+        }
+        printf("\n");
+        
+        return 0;
+    }
+}

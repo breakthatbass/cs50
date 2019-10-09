d67f7db95c823ca9fcce668094a03e3379311af4
breakthatbass <gamache.taylor@gmail.com>
2019-10-08 20:41:18 -0400

    update mario problem to hard version

--- mario.c
+++ mario.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+
 int main(void)
 {
     int num;
@@ -13,15 +14,31 @@ int main(void)
     // if number is between 1 and 8, proceed to loops
     for (int i = 0; i < num; i++)
     { 
+        
+        // this loop just prints the spaces
         for(int j = 0; j < num - i - 1; j++)
         {
-            printf(" ");
+            printf(" "); 
         }
+        // this loop prints the #
         for(int k = 0; k < i + 1; k++)
         {
             printf("#");
         }
-        printf("\n");
+        // this printf is inbetween the loops to give a space between the pyramids
+        printf("   ");
+        
+        // the next two loops are the same as the top just inverted.
+        for(int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+       
+        for(int j = 0; j < num - i - 1; j++)
+        {
+            printf(" ");
+        }
+        // then go to the next line and repeat
+         printf("\n");
     }
 }
-// finally got it to work :) 

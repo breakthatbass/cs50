260bc095e3a87eedb25a5967f9e24e4035e5a2e1
breakthatbass <gamache.taylor@gmail.com>
2019-11-13 08:19:24 -0500

    update caesar.py

--- Python/caesar.py
+++ Python/caesar.py
@@ -1,9 +1,22 @@
 # cs50 pset 6
 from cs50 import get_string
 from sys import argv
+from string import *
+import sys
 
-def main():
+# converts the word into ints
+k = atoi(sys.argv[1]) % 26 # currently not working...will do later
 
+# if there is only one extra command line arg, ...
+if len(sys.argv) == 2:
+    s = get_string("Plaintext:")
+    print("Cyphertext:")
 
-if __name__ == "__main__":
-    main()
+    #loop through each char in str and add the k ints to it
+    for i in range(len(s)):
+
+        c = s[i] + k
+        print("ciphertext:", c)
+
+else:
+    print("Usage: ./caesar key");

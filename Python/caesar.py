91e86a7c74a0f62e4cdd674ec276ef3175ca58b1
breakthatbass <gamache.taylor@gmail.com>
2019-11-15 08:07:57 -0500

    Fix caesar.py bugs

--- Python/caesar.py
+++ Python/caesar.py
@@ -1,18 +1,29 @@
 # cs50 pset 6
 from cs50 import get_string
-from sys import argv
-from string import *
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: ./caesar key")
 
-# converts the word into ints
-key = sys.argv[1] % 26 # currently not working...will do later
-word = get_string("Plaintext:")
-print("Cyphertext:")
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: ./caesar key") # Remind user to giv an argument
+
+    # converts the word into int
+    key = int(sys.argv[1])
+
+    word = get_string("Plaintext: ")
+    print("Cyphertext: ", end="")
 
     #loop through each char in str and add the k ints to it
-for i in range(len(word)):
-    c = word[i] + key
-    print(c)
+    fix = 65 if word.isupper() else 97
+    for i in range(len(word)):
+
+        # convert strings to unicode numbers
+        a = ord(word[i])
+        b = ((((a - fix) + key) % 26) + fix)
+        c = chr(b)
+        print(c, end="")
+
+    print()
+
+if __name__ == "__main__":
+    main()

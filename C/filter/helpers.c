e7cafe79dafe8fa2d926dc4f9a9afaca053829ec
breakthatbass <gamache.taylor@gmail.com>
2020-01-10 19:53:12 -0500

    add grayscale function...it finally works

--- C/filter/helpers.c
+++ C/filter/helpers.c
@@ -1,25 +1,44 @@
 #include "helpers.h"
+#include <math.h>
+
+void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 
 // Convert image to grayscale
-void grayscale(int height, int width, RGBTRIPLE image[height][width])
+int main (void)
 {
-    return;
+
+    
 }
 
-// Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width])
-{
-    return;
-}
+    void grayscale(int height, int width, RGBTRIPLE image[height][width])
+    {
 
-// Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width])
-{
-    return;
-}
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                int avg = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+                image[i][j].rgbtRed = avg;
+                image[i][j].rgbtGreen = avg;
+                image[i][j].rgbtBlue = avg;
+            }
+        }
+    }
 
-// Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width])
-{
+    // Convert image to sepia
+    void sepia(int height, int width, RGBTRIPLE image[height][width])
+    {
     return;
-}
+    }
+
+    // Reflect image horizontally
+    void reflect(int height, int width, RGBTRIPLE image[height][width])
+    {
+    return;
+    }
+
+    // Blur image
+    void blur(int height, int width, RGBTRIPLE image[height][width])
+    {
+    return;
+    }

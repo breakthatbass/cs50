0e7c7dd83d7ce745555ed20de70d4976620d09f6
breakthatbass <gamache.taylor@gmail.com>
2020-01-10 21:00:26 -0500

    add sepia filter function

--- C/filter/helpers.c
+++ C/filter/helpers.c
@@ -4,12 +4,12 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 
 
-
 int main (void)
 {
     // this file wouldn't compile without the main function...even an empty one!
     
 }
+
 
     // Convert image to grayscale
     void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -30,7 +30,61 @@ int main (void)
     // Convert image to sepia
     void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
-    return;
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                int red = round((image[i][j].rgbtRed * 0.393) + (image[i][j].rgbtGreen * 0.769) + (image[i][j].rgbtBlue * 0.189));
+                int green = round((image[i][j].rgbtRed * 0.349) + (image[i][j].rgbtGreen * 0.686) + (image[i][j].rgbtBlue * 0.168));
+                int blue = round((image[i][j].rgbtRed * 0.272) + (image[i][j].rgbtGreen * 0.534) + (image[i][j].rgbtBlue * 0.232));
+
+                // check to see if the values are between 0 and 255
+                // red
+                if (red > 255)
+                {
+                    image[i][j].rgbtRed = 255;
+                }
+                else if (red < 0)
+                {
+                    image[i][j].rgbtRed = 0;
+                }
+                else
+                {
+                    image[i][j].rgbtRed = red;
+                }
+
+
+                // green
+                if (green > 255)
+                {
+                    image[i][j].rgbtGreen = 255;
+                }
+                else if (green < 0)
+                {
+                    image[i][j].rgbtGreen = 0;
+                }
+                else
+                {
+                    image[i][j].rgbtGreen = green;
+                }
+
+
+                // blue
+                if (blue > 255)
+                {
+                    image[i][j].rgbtBlue = 255;
+                }
+                else if (blue < 0)
+                {
+                    image[i][j].rgbtBlue = 0;
+                }
+                else
+                {
+                    image[i][j].rgbtBlue = blue;
+                }
+
+            }
+        }
     }
 
     // Reflect image horizontally
